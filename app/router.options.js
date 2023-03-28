0dafbd73bbe03b7c909615a71cccdf6c6a96b3e3
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-03-28 12:57:23 +0530

    fix: scrollToTop on navigation

--- app/router.options.js
+++ app/router.options.js
@@ -1,6 +1,6 @@
-// This is a workaround for the issue with the scrollBehavior still present in Nuxt 3.0
+// This is a workaround for the issue with the scrollBehavior still present in Nuxt 3.3.1
 export default {
   scrollBehavior () {
-    return false
+    return { top: 0 }
   }
 }
